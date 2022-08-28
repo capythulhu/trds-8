ec2ab76b6cd4c1f7dece4ee33295ce11becce0a2
thzoid <thiagozoid@gmail.com>
2022-08-28 11:19:56 -0300

    remove unsigned conversion

--- inst.go
+++ inst.go
@@ -1,24 +1,16 @@
 package trds16
 
-// Int8 to byte
-func abs(n int8) byte {
-	if n < 0 {
-		return byte(-n)
-	}
-	return byte(n)
-}
-
 // Build instruction
 func I(op byte, arg ...int8) uint16 {
 	if len(arg) > 0 {
-		return uint16(op)<<8 | uint16(arg[0])
+		return uint16(op)<<8 | uint16(byte(arg[0]))
 	}
 	return uint16(op) << 8
 }
 
 // Get operation from instruction
 func Op(inst uint16) byte {
-	return abs(int8(inst >> 8))
+	return byte(inst >> 8)
 }
 
 // Get value from instruction
